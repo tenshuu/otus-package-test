122d5a5807dfc37587b76a35058d36e87b585040
vdanilova <vdanilova@marfa-tech.com>
2025-06-13 17:52:15 +0300

    first commit

---
+++ src/App/Service/TextService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenshuu\OtusPackageTest\App\Service;
+
+class TextService
+{
+    public function getText(): string
+    {
+        return 'SUCCESS';
+    }
+}
